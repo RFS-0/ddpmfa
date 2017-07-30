8ff5e1516bc2841d77d771df480511e04175c325
Remo Schenker <remo@mountain-yard.ch>
2017-07-31 00:01:14 +0200

    Some experiments with views and templates

--- ddpmfa/dpmfa/views.py
+++ ddpmfa/dpmfa/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Model
 
@@ -13,15 +13,27 @@ def project_administration(request):
 def project_overview(request, project_name):
     return HttpResponse("You are at the project overview site of project: %s" % project_name)
 
-def model_overview(request, project_name):
+def model_overview(request, model_name):
+    primary_key = model_name
+    model = get_object_or_404(Model, pk=primary_key)
+    model_attributes = {} 
+    model_attributes['Name of the model: '] = model.name
+    model_attributes['Description of the model: '] = model.description
+    context = {
+    'primary_key': primary_key,
+    'model_attributes': model_attributes,
+    }
+    
+    return render(request, 'dpmfa/model_overview.html', context)
+
+"""def model_configuratio(request, model_name):
     try:
-        model = Model.objects.get(pk=project_name)
-        latest_model_list = Model.objects.order_by('-evt_created')[:5]
+        model = Model.objects.get(pk=model_name)
         context = {
-        'latest_model_list': latest_model_list,
-        }
-    
+        'model_attributes': model_attributes,
+            }
+          
     except Model.DoesNotExist:
-        raise Http404("Model does not exist")
+        raise Http404("This model does not exist.")
     
-    return render(request, 'dpmfa/index.html', context)
+    return render(request, )"""
