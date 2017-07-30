51617b73bc1d072c739df24e789590db5002357e
Remo Schenker <remo@mountain-yard.ch>
2017-07-30 21:16:44 +0200

    Created first template

--- ddpmfa/dpmfa/views.py
+++ ddpmfa/dpmfa/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from django.shortcuts import render
+
 from .models import *
 
 def home(request):
@@ -13,6 +16,7 @@ def project_overview(request, project_name):
 
 def model_overview(request, project_name):
     latest_model_list = Model.objects.order_by('-evt_created')[:5]
-    ','.join([m.name for  m in latest_model_list])
-    return HttpResponse("You are at the model over view site for project: %s"
-                        "The latest models you created are: %s" % (project_name, latest_model_list))
+    context = {
+        'latest_model_list': latest_model_list,
+        }
+    return render(request, 'dpmfa/index.html', context)
