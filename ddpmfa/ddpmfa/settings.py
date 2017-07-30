9d06867825d4b2ae080b2ebd3da89ef3b8fc8968
Remo Schenker <remo@mountain-yard.ch>
2017-07-30 14:36:31 +0200

    Included the dpmfa app in the ddpmfa project settings

--- ddpmfa/ddpmfa/settings.py
+++ ddpmfa/ddpmfa/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dpmfa.apps.DpmfaConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
