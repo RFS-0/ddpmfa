b7ca3dbb6a2abbb386109cadb560c2ed4062c7ff
Remo Schenker <remo@mountain-yard.ch>
2017-07-30 17:45:11 +0200

    Added urls and views for the home page, project administration etc.

--- ddpmfa/dpmfa/urls.py
+++ ddpmfa/dpmfa/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # ex: /home/
+    url(r'^$', views.home, name='home'),
+    # ex: /administration/
+    url(r'^administration/$', views.project_administration, name='project_administration'),
+    # ex: /some_project_name/
+    url(r'^(?P<project_name>[a-z0-9_]+)/$', views.project_overview, name='project_overview'),
+    # ex: /some_project_name/model_overview
+    url(r'^(?P<project_name>[a-z0-9_]+)/model_overview$', views.model_overview, name='model_overview'),
     ]
--- ddpmfa/dpmfa/views.py
+++ ddpmfa/dpmfa/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Hello world. You are at the dpmfa index")
+def home(request):
+    return HttpResponse("You are at the home page. This is site provides information about the WebApp.")
+
+def project_administration(request):
+    return HttpResponse("You are at the project administration site. Here you can add and edit projects.")
+
+def project_overview(request, project_name):
+    return HttpResponse("You are at the project overview site of project: %s" % project_name)
+
+def model_overview(request, project_name):
+    return HttpResponse("You are at the model over view site for project: %s" % project_name)
