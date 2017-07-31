81a9c8b252fd710630f9a95b42b2ec3b30dae8e4
Remo Schenker <remo@mountain-yard.ch>
2017-07-31 15:39:19 +0200

    Renamed db tables

--- ddpmfa/dpmfa/models.py
+++ ddpmfa/dpmfa/models.py
@@ -3,7 +3,7 @@ from django.db.models.fields import CharField
 from django.core.validators import int_list_validator
 from django.utils import timezone
 
-class Compartment(models.Model):
+class compartment(models.Model):
     name = models.CharField(verbose_name='compartment: ', default='compartment', max_length=250)
     evt_created = models.DateTimeField(verbose_name='time of creation of the compartment', auto_now_add=True)
     compNumber = models.BigIntegerField(verbose_name='number of the compartment')
@@ -13,7 +13,7 @@ class Compartment(models.Model):
     def __str__(self):
         return self.name + " " + str(self.pk)
     
-class FlowCompartment(Compartment):
+class flow_compartment(compartment):
     name_of_flow_compartment = models.CharField('flow compartment', default='flow compartment', max_length=250)
     adjustOutgoingTCs = models.BooleanField(verbose_name='adjust outgoing TCs of this compartment', default=True)
     logOutflows = models.BooleanField(verbose_name='log outflows from this compartment', default=True)
@@ -22,13 +22,13 @@ class FlowCompartment(Compartment):
     def __str__(self):
         return self.name + " " + str(self.pk)
     
-class Stock(FlowCompartment):
+class stock(flow_compartment):
     name_of_stock = models.CharField(verbose_name='stock', default='stock', max_length=250)
     
     def __str__(self):
         return self.name_of_stock.verbose_name + self.pk
     
-class LocalRelease(models.Model):
+class local_release(models.Model):
     # TODO [rse]: should be FK field
     name = CharField(verbose_name='local release: ', default='local release', max_length=250)
     releaseList = CharField(verbose_name='a list of with a release for each period', validators=[int_list_validator()], max_length=250)
@@ -39,24 +39,24 @@ class LocalRelease(models.Model):
     def __str__(self):
         return self.name.verbose_name + self.pk
     
-class FixedRateRelease(LocalRelease):
+class fixed_rate_release(local_release):
     delay = models.SmallIntegerField(verbose_name='delay time in periods before release starts')
     releaseRate = models.FloatField(verbose_name='periodic release rate')
     
     
-class ListRelease(LocalRelease):
+class list_release(local_release):
     delay = models.SmallIntegerField(verbose_name='delay time in periods before release starts')
     # TODO [all]: we will have to implement a float list validator
     releaseRateList = models.CharField(verbose_name='list of release rates of a stored material in future periods', validators=[int_list_validator()], max_length=250)
     
-class FunctionRelease(LocalRelease):
+class function_release(local_release):
     delay = models.SmallIntegerField(verbose_name='delay time in periods before release starts')
     # TODO [all]: we will have to implement a function field to store functions
     releaseFunction = models.CharField(verbose_name='probability density function probability distribution function (e.g. from scipy.stats) to sample random values for the transfer coefficient', max_length=250)
     
     
-class Transfer(models.Model):
-    target=models.ForeignKey(to=Compartment, on_delete=models.CASCADE)
+class transfer(models.Model):
+    target=models.ForeignKey(to=compartment, on_delete=models.CASCADE)
     
     name = models.CharField(verbose_name='transfer: ', default='transfer', max_length=250)
     priority = models.SmallIntegerField(verbose_name='priority of the transfer')
@@ -65,14 +65,14 @@ class Transfer(models.Model):
     def __str__(self):
         return self.name.verbose_name + self.pk
     
-class ConstantTransfer(Transfer):
+class constant_transfer(transfer):
     name_of_constant_transfer = models.CharField(verbose_name='constant transfer: ', default='constant transfer', max_length=250)
     value = models.FloatField(verbose_name='deterministic value for the transfer coefficient')
     
     def __str__(self):
         return self.name_of_constant_transfer.verbose_name + self.pk
     
-class RandomChoiceTransfer(Transfer):
+class random_choice_transfer(transfer):
     name_of_random_choice_transfer = models.CharField(verbose_name='random choice transfer: ', default='random choice transfer', max_length=250)
     # TODO [all]: we will have to implement a float list validator 
     sample = models.CharField(verbose_name='a given sample of values from which is randomly drawn', validators=[int_list_validator()], max_length=250)
@@ -80,7 +80,7 @@ class RandomChoiceTransfer(Transfer):
     def __str__(self):
         return self.name_of_random_choice_transfer.verbose_name + self.pk
     
-class StochasticTransfer(Transfer):
+class stochastic_transfer(transfer):
     name_of_stochastic_transfer = models.CharField(verbose_name='stochastic transfer: ', default='stochastic transfer', max_length=250)
     parameters = models.CharField(verbose_name='parameter list of the probability distribution function', validators=[int_list_validator()], max_length=250)
     # TODO [all]: we will have to implement a function field to store functions
@@ -89,8 +89,8 @@ class StochasticTransfer(Transfer):
     def __str__(self):
         return self.name_of_stochastic_transfer.verbose_name + self.pk
     
-class AggregatedTransfer(Transfer):
-    singleTransfers = models.ForeignKey(to=Transfer, related_name='Transfer', verbose_name='a list of SochasticTransfers and/or RandomChoiceTransfers to be considered in the combined distribution', on_delete=models.CASCADE, max_length=250)  
+class aggregated_transfer(transfer):
+    singleTransfers = models.ForeignKey(to=transfer, related_name='transfer', verbose_name='a list of SochasticTransfers and/or RandomChoiceTransfers to be considered in the combined distribution', on_delete=models.CASCADE, max_length=250)  
     
     name_of_aggregated_transfer = models.CharField(verbose_name='aggregated transfer: ', default='aggregated transfer', max_length=250)
     # TODO [all]: we will have to implement a float list validator
@@ -99,8 +99,8 @@ class AggregatedTransfer(Transfer):
     def __str__(self):
         return self.name_of_aggregated_transfer.verbose_name + self.pk
     
-class ExternalInflow(models.Model):
-    target=models.ForeignKey(to=Compartment, on_delete=models.CASCADE)
+class external_inflow(models.Model):
+    target=models.ForeignKey(to=compartment, on_delete=models.CASCADE)
     
     name = models.CharField(verbose_name=' external inflow: ', default='External Inflow', max_length=250)
     startDelay = models.SmallIntegerField(verbose_name='delay time in periods before release starts')
@@ -112,37 +112,37 @@ class ExternalInflow(models.Model):
     def __str__(self):
         return self.name.verbose_name + self.pk
         
-class ExternalListInflow(ExternalInflow):
-    valueList = models.ForeignKey(to='SinglePeriodInflow', verbose_name='name of the single period inflow') 
+class external_list_inflow(external_inflow):
+    valueList = models.ForeignKey(to='single_period_inflow', verbose_name='name of the single period inflow') 
     
     name_of_external_list_inflow = models.CharField(verbose_name='name of the external list inflow', default='external list inflow', max_length=250)
     
     def __str__(self):
         return self.name_of_external_list_inflow.verbose_name + self.pk
     
-class ExternalFunctionInflow(ExternalInflow):
+class external_function_inflow(external_inflow):
     name_of_external_function_inflow = models.CharField(verbose_name='name of external function inflow', default='external function inflow', max_length=250)
     # TODO [all]: we will have to implement a function field to store functions
     inflowFunction = models.CharField(verbose_name='probability density function probability distribution function (e.g. from scipy.stats) to sample random values for the transfer coefficient', max_length=250)
-    basicInflow = models.ForeignKey(to='SinglePeriodInflow', verbose_name='name of the single period inflow')
+    basicInflow = models.ForeignKey(to='single_period_inflow', verbose_name='name of the single period inflow')
     
     def __str__(self):
         return self.name_of_external_function_inflow.verbose_name + self.pk
     
 
-class SinglePeriodInflow(ExternalListInflow):
+class single_period_inflow(external_list_inflow):
     currentValue = models.BigIntegerField(verbose_name='current inflow value')
     
-class FixedValueInflow(SinglePeriodInflow):
+class fixed_value_inflow(single_period_inflow):
     value = models.FloatField(verbose_name='the inflow value')
     
-class StochasticInflow(SinglePeriodInflow):
+class stochastic_inflow(single_period_inflow):
     name_of_stochastic_inflow = models.CharField(verbose_name='stochastic inflow', default='stochastic inflow', max_length=250)
     
     def __str__(self):
         return self.name.verbose_name + self.pk
     
-class RandomChoiceInflow(SinglePeriodInflow):
+class random_choice_inflow(single_period_inflow):
     name_of_random_choice_inflow = models.CharField(verbose_name='random choice inflow', default='random choice inflow', max_length=250)
     # TODO [all]: we will have to implement a float list validator
     sample = models.CharField(verbose_name='a given sample of values from which is randomly drawn', validators=[int_list_validator()], max_length=250)
@@ -150,9 +150,9 @@ class RandomChoiceInflow(SinglePeriodInflow):
     def __str__(self):
         return self.name_of_random_choice_inflow.verbose_name + self.pk
                 
-class Model(models.Model):
-    compartment = models.ForeignKey(to=Compartment, on_delete=models.CASCADE)
-    inflow = models.ForeignKey(to=ExternalInflow, on_delete=models.CASCADE)
+class model(models.Model):
+    compartment = models.ForeignKey(to=compartment, on_delete=models.CASCADE)
+    inflow = models.ForeignKey(to=external_inflow, on_delete=models.CASCADE)
     
     name = models.CharField(verbose_name='name of the model', primary_key=True, max_length=250)
     description = models.TextField(verbose_name='description of this model')
