9c11b1f0815f5b990913bea044f63af871439c31
Manuel Kunz <mku.computergraphicslab@gmail.com>
2017-08-01 12:12:29 +0200

    Added home url

--- ddpmfa/dpmfa/urls.py
+++ ddpmfa/dpmfa/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'dpmfa'
 urlpatterns = [
     # ex: /home/
-    url(r'^$', views.home, name='home'),
+    url(r'(?:^$)|(?:^home/$)', views.home, name='home'),
     # ex: /administration/
     url(r'^administration/$', views.project_administration, name='project_administration'),
     # ex: /some_project_name/
@@ -13,4 +13,5 @@ urlpatterns = [
     url(r'^(?P<model_name>[a-zA-Z0-9_]+)/model_overview$', views.model_overview, name='model_overview'),
     # ex: /some_project_name/configuration
     url(r'^(?P<model_name>[a-zA-Z0-9_]+)/configuration', views.model_overview, name='model_configuration'),
+
     ]
