ddd74ec1790a0dc0d0ae035c071d851444eb196c
Remo Schenker <remo@mountain-yard.ch>
2017-08-06 11:06:03 +0200

    Added file for conversion of django <-> dpmfa

---
+++ ddpmfa/dpmfa/converter.py
@@ -0,0 +1,5 @@
+# imports are correct but do not work, because package is written python version 2
+import dpmfa.models as django_models
+from dpmfa.dpmfa_simulator_0_921.dpmfa_simulator import components as package_components
+from dpmfa.dpmfa_simulator_0_921.dpmfa_simulator import model as package_model
+from dpmfa.dpmfa_simulator_0_921.dpmfa_simulator import simulator as package_simulator
