e82ceacdc45064afa36b5dd79634ebb6796e0418
Remo Schenker <remo@mountain-yard.ch>
2017-08-06 16:59:32 +0200

    Added class to convert flow compartment from db to dpmfa flow compartment

--- ddpmfa/dpmfa/converter.py
+++ ddpmfa/dpmfa/converter.py
@@ -5,22 +5,57 @@ from dpmfa.dpmfa_simulator_0_921.dpmfa_simulator import simulator as package_sim
 
 class CompartmentConverter(object):
     
-    def __init__(self, db_compartment):
-        self.compartment_db = db_compartment
-        self.compNumber = db_compartment.pk
-        self.name = db_compartment.name
-        self.logInflows = db_compartment.log_inflows
-        self.categories = db_compartment.categories
-        self.compartment_dpmfa = package_components.Compartment(
-            self.name, 
-            self.logInflows, 
-            self.categories
-            )
-                
-        #except:
-        #    print("Could not convert the entity received from the db")
+    def __init__(self, db_compartment=django_models.compartment):
+        try: 
+            self.compartment_db = db_compartment
+            self.compNumber = db_compartment.pk
+            self.name = db_compartment.name
+            self.logInflows = db_compartment.log_inflows
+            self.categories = db_compartment.categories
             
-    def getDpmfaCompartment(self):
+            self.compartment_dpmfa = package_components.Compartment(
+                self.name, 
+                self.logInflows, 
+                self.categories
+                )
+                
+        except:
+            print("Could not convert the entity received from the db to dpmfa compartment")
+            
+    def getCompartment(self):
         return self.compartment_dpmfa
+    
+class FlowCompartmentConverter(CompartmentConverter):
+    
+    def __init__(self, db_flow_compartment=django_models.flow_compartment):
+        super(FlowCompartmentConverter, self).__init__(db_flow_compartment)
         
+        try:      
+            self.transfers = []        
+            self.adjustOutTCs = db_flow_compartment.adjust_outgoing_tcs
+            self.logOutflows = db_flow_compartment.log_outflows
+            self.immediateReleaseRate = 1
+            
+            self.flow_compartment_dpmfa = package_components.FlowCompartment(
+                self.name, 
+                self.transfers, 
+                self.logInflows, 
+                self.logOutflows, 
+                self.adjustOutTCs, 
+                self.categories
+                )
         
+        except:
+            print("Could not convert the entity received from the db to dpmfa flow compartment")
+     
+        
+    def getFlowCompartment(self):
+        return self.flow_compartment_dpmfa
+    
+    
+    
+    
+    
+    
+    
+    
